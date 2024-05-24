b6ce998d68893dadd5e98d06a666169cda5a60d8
Kanyawadee kongkaew <kanyawadeekongkaew@partykywdd.local>
2024-05-24 15:59:28 +0700

    finish section cart

--- main.js
+++ main.js
@@ -51,5 +51,85 @@ function displayProduct(product) {
   dashboardsection.appendChild(card);
 }
 
-///
+function toggleSelector(event) {
+  const checkbox = event.target;
+  const checkboxId = parseInt(checkbox.getAttribute("data-id"));
+  const product = products.find((product) => product.id === checkboxId);
 
+  if (checkbox.checked) {
+    product.checked = true;
+  } else {
+    product.checked = false;
+  }
+}
+
+document.getElementById("addbtn").addEventListener("click", () => {
+  cart = products.filter((product) => product.checked);
+  dp_cart(cart);
+});
+
+function dp_cart(cart) {
+  const displayDiv = document.getElementById("dp_cart");
+  displayDiv.innerHTML = "";
+
+  cart.forEach((product) => {
+    const div = document.createElement("div");
+    div.className = "items-center bg-white p-4 rounded-lg shadow-lg";
+
+    div.innerHTML = `
+     <input type="checkbox" class="sr-only w-5 h-5 text-2xl accent-rose-600" data-id="${product.id}" onchange="calculateSelector(event)">
+     <img src="${product.img_url}" alt="${product.product_name} class="w-full aspect-[4/3] rounded-md mb-4 object-cover">
+     
+     <div class="flex items-center justify-between">
+ 
+     <div class="mt-3 ">
+     <span class="block ml-2 text-gray-700 font-semibold text-3xl">${product.product_name}</span>
+     <span class="block ml-2 text-gray-700 font-semibold text-2xl">${product.price}$</span>
+     </div>
+     <button
+     type="submit"
+     id="${product.id}"
+     onclick="removeCard(${product.id})"
+     class="bg-blue-500 px-4 text-white rounded-md hover:bg-blue-600 hover:ring hover:ring-blue-300 py-4 mt-2"
+   >Remove
+   </button>
+     </div>
+     `;
+    displayDiv.appendChild(div);
+  });
+  const btnDiv = document.getElementById("btnCal");
+  btnDiv.innerHTML = `<button id="cfp" onclick="calulateBtn()" class="bg-gray-500 px-4 text-white rounded-md hover:bg-gray-600 hover:ring hover:ring-gray-300">Calculate Final Price</button>`;
+
+  const totalDiv = document.getElementById("total");
+  totalDiv.innerHTML = `<h2 id="total" class="text-xl mt-3">You have to pay:</h2>`;
+}
+
+function removeCard(productId) {
+  cart = cart.filter((product) => product.id !== productId);
+  dp_cart(cart);
+}
+// function calculateSelector(event) {
+//   const checkbox = event.target;
+//   const checkboxId = parseInt(checkbox.getAttribute("data-id"));
+//   const product = products.find((product) => product.id === checkboxId);
+
+//   if (checkbox.checked) {
+//     product.checked = true;
+//   } else {
+//     product.checked = false;
+//   }
+// }
+
+function calulateBtn() {
+  // cart = products.filter((product) => product.checked && product.id !== -1);
+  calculateFinal(cart);
+}
+
+function calculateFinal(cart) {
+  let sum = 0;
+  cart.forEach((product) => {
+    sum += parseFloat(product.price);
+  });
+  const totalPrice = document.getElementById("total");
+  totalPrice.textContent = `You have to pay: ${sum.toFixed(2)} $`;
+}
